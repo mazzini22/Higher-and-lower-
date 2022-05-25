e8af95914dee6ef4e52be63f3af624999459212c
Breno Mazzini Costa <99024173+mazzini22@users.noreply.github.com>
2022-05-25 20:39:45 -0200

    files cs

---
+++ Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace _23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lblmaior = new System.Windows.Forms.Label();
+            this.lblmenor = new System.Windows.Forms.Label();
+            this.lblsoma = new System.Windows.Forms.Label();
+            this.lblmedia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1° Número";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(115, 94);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(105, 20);
+            this.txt1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(379, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "checar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(378, 94);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(105, 20);
+            this.txt2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(374, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2° Número";
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(644, 94);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(105, 20);
+            this.txt3.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(640, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(109, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "3° Número";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(645, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(111, 393);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 46);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lblmaior
+            // 
+            this.lblmaior.AutoSize = true;
+            this.lblmaior.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmaior.Location = new System.Drawing.Point(69, 237);
+            this.lblmaior.Name = "lblmaior";
+            this.lblmaior.Size = new System.Drawing.Size(15, 24);
+            this.lblmaior.TabIndex = 9;
+            this.lblmaior.Text = ".";
+            // 
+            // lblmenor
+            // 
+            this.lblmenor.AutoSize = true;
+            this.lblmenor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmenor.Location = new System.Drawing.Point(301, 237);
+            this.lblmenor.Name = "lblmenor";
+            this.lblmenor.Size = new System.Drawing.Size(15, 24);
+            this.lblmenor.TabIndex = 10;
+            this.lblmenor.Text = ".";
+            // 
+            // lblsoma
+            // 
+            this.lblsoma.AutoSize = true;
+            this.lblsoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsoma.Location = new System.Drawing.Point(511, 237);
+            this.lblsoma.Name = "lblsoma";
+            this.lblsoma.Size = new System.Drawing.Size(15, 24);
+            this.lblsoma.TabIndex = 11;
+            this.lblsoma.Text = ".";
+            // 
+            // lblmedia
+            // 
+            this.lblmedia.AutoSize = true;
+            this.lblmedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmedia.Location = new System.Drawing.Point(725, 237);
+            this.lblmedia.Name = "lblmedia";
+            this.lblmedia.Size = new System.Drawing.Size(15, 24);
+            this.lblmedia.TabIndex = 12;
+            this.lblmedia.Text = ".";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 487);
+            this.Controls.Add(this.lblmedia);
+            this.Controls.Add(this.lblsoma);
+            this.Controls.Add(this.lblmenor);
+            this.Controls.Add(this.lblmaior);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label lblmaior;
+        private System.Windows.Forms.Label lblmenor;
+        private System.Windows.Forms.Label lblsoma;
+        private System.Windows.Forms.Label lblmedia;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n1 = Convert.ToDouble(txt1.Text);
+            double n2 = Convert.ToDouble(txt2.Text);
+            double n3 = Convert.ToDouble(txt3.Text);
+
+            // verificando qual é o maior
+           if (n1 > n2)
+            {
+                if (n1 > n3)
+                {
+                    lblmaior.Text = n1 + " é maior";
+                }
+                else
+                {
+                    lblmaior.Text = n3 + " é maior";
+                }
+                
+            }
+            else
+            {
+                if (n2 > n3)
+                {
+                    lblmaior.Text = n2 + " é maior";
+                }
+                else
+                {
+                    lblmaior.Text = n3 + " é maior";
+                }
+            }
+            // fim verificação
+
+            // menor
+            if (n1 < n2)
+            {
+                if (n1 < n3)
+                {
+                    lblmenor.Text = n1 + " é menor";
+                }
+                else
+                {
+                    lblmenor.Text = n3 + " é menor";
+                }
+
+            }
+            else
+            {
+                if (n2 < n3)
+                {
+                    lblmenor.Text = n2 + " é menor";
+                }
+                else
+                {
+                    lblmenor.Text = n3 + " é menor";
+                }
+            }
+            //fim da verificação
+            
+            //soma
+            lblsoma.Text = "soma: " + Convert.ToString(n1 + n2 + n3);
+            //
+
+            //media
+            lblmedia.Text = "media: " + Convert.ToString((n1 + n2 + n3) / 3);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "";
+            txt2.Text = "";
+            txt2.Text = "";
+            lblmaior.Text = "";
+            lblmenor.Text = "";
+            lblsoma.Text = "";
+            lblmedia.Text = "";
+            txt1.Focus();
+
+        }
+    }
+}
